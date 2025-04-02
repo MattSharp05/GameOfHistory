384f7dd9acbc4239f218e293b4735d73d704d6e6
MattSharp05 <m5sharp@icloud.com>
2025-04-01 23:09:44 -0400

    added path to game modes

---
+++ src/app/modes/picture/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from './page.module.css';
+
+export default function PictureGame() {
+  return (
+    <div className={styles.container}>
+      <h1>Picture Game Mode</h1>
+      {/* Game content will go here */}
+    </div>
+  );
+} 
